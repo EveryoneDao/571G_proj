ea34e5b51749f8d245660391ce0c054255e44d58
Mengmeng <mengmengli86@outlook.com>
2022-03-25 22:30:02 -0700

    time to get rid of hello world for now

--- src/util/interact.js
+++ src/util/interact.js
@@ -1,6 +1,25 @@
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
 //export const helloWorldContract;
 
-export const loadCurrentMessage = async () => { 
+// const contractABI = require("../contract-abi.json");
+// const contractAddress = "0x752b5fDB8012c26940B00ec19Dac749Bf66F10db";
+// export const pollContract = new web3.eth.Contract(
+//     contractABI,
+//     contractAddress
+// );
+/* 
+This function handles the logic of loading current events
+stored in the smart contract. It will make a read call 
+to the poll smart contract using the Alchemy Web3 API
+*/
+export const loadCurrentEvent = async () => { 
+  
+};
+
+export const loadAllEvents = async () => { 
   
 };
 
@@ -12,6 +31,10 @@ export const getCurrentWalletConnected = async () => {
   
 };
 
-export const updateMessage = async (address, message) => {
+// export const updateEventList = async (address, message) => {
   
-};
+// };
+
+// export const createNewEvent = async (address, message) => {
+  
+// };
